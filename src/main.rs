11c9478ac051765cb863e5177b2f70949e738c2b
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-30 20:54:23 -0300

    Variables in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_name= "Alessandra";
+    my_name = "John";
 }
+
+// By default, variables are immutable
+
+// Muttable variables must be explicitly declared 
+// with the mut key word.
